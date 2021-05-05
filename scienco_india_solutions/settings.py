0f4835dc43f89b641d2b530639075577faac0a34
pulkit <pulkitmadaanit@gmail.com>
2021-05-05 23:59:03 +0530

    allowed host

--- scienco_india_solutions/settings.py
+++ scienco_india_solutions/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 't1af&6hcv#z-96fie!@-%9k4vu&sar$kgnvtby9qzo1u=ln!8$'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
